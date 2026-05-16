d11a691479100c53ed0ab4c572f1e366fc33adf8
Zaki <zakihakim385@gmail.com>
2026-05-16 22:25:37 +0700

    initial commit

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        mason = true,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
